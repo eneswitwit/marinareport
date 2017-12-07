7461f6dda7504345a00626cd339069a875bc02bc
Enes Witwit <witwit@stud.uni-heidelberg.de>
2017-12-07 16:16:39 +1100

    Edited PDF

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,1 +1,21 @@
-{{ dd('die') }}
+<div style="height:1188px;width: 803px;">
+Page 1
+
+
+</div>
+<div style="height:1188px;width: 803px;">
+Page 2
+
+</div>
+<div style="height:1188px;width: 803px;">
+Page 3
+
+
+</div>
+<div style="height:1188px;width: 803px;">
+Page 4
+
+</div>
+
+
+
