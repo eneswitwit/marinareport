003e85e21035dbb680c8ed0c5528abec5418ad8f
Enes Witwit <witwit@stud.uni-heidelberg.de>
2017-12-13 23:08:30 +1100

    Update 2017_12_11_020505_add_ansprechpartner_.php

--- database/migrations/2017_12_11_020505_add_ansprechpartner_.php
+++ database/migrations/2017_12_11_020505_add_ansprechpartner_.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAnsprechpartner extends Migration
+class AddAnsprechpartners extends Migration
 {
     /**
      * Run the migrations.
